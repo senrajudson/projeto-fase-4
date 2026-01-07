b1fcf7436d6e42b81ba18622b2a883a38cad0e75
senrajudson <judsonnascimento.filho@gmail.com>
2026-01-07 20:23:57 -0300

    fixes - main_api import app fastapi

--- src/main_api.py
+++ src/main_api.py
@@ -67,4 +67,4 @@ if __name__ == "__main__":
 
     host = os.getenv("API_HOST", "0.0.0.0")
     port = int(os.getenv("API_PORT", "9010"))
-    uvicorn.run("src.main:app", host=host, port=port, reload=True)
+    uvicorn.run("src.main_api:app", host=host, port=port, reload=True)
